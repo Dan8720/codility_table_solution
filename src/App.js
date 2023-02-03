84cf30dc0c6a98508e0708240ca1fc918e06aff6
Dan Johnston <dan.johnston@credera.co.uk>
2023-02-03 11:19:33 +0000

    What you ended up with finally on the test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+const USERS_URL = 'https://localhost:4000/users';
+
+const [users, setUsers] = useState([]);
+const [currentPage, setCurrentPage] = useState(1);
+const [perPage] = useState(10);
+
+useEffect(() => {
+  fetch(`${USERS_URL}=${currentPage}&limit=${perPage}`)
+      .then(response => response.json())
+      .then(data => setUsers(data.users))
+      .catch(error => console.log(error));
+}, [currentPage, perPage]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <table className="table">
+          <thead>
+          <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+          </tr>
+          </thead>
+          <tbody>
+
+          {users.map(user => (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.firstName}</td>
+                <td>{user.lastName}</td>
+              </tr>
+          ))}
+
+          </tbody>
+        </table>
+        <section className="pagination">
+          <button className="first-page-btn">first</button>
+          <button className="previous-page-btn">previous</button>
+          <button className="next-page-btn">next</button>
+          <button className="last-page-btn">last</button>
+        </section>
+      </div>
   );
 }
 
